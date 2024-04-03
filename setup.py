a3f11aa287fdd2d81ac1707075efd3873399cd76
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2024-04-03 17:05:36 +0300

    cleanup

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
     description="EEA Copernicus InSitu Plone backend policy",
     long_description_content_type="text/x-rst",
     long_description=(
-        open("README.md").read()
+        open("README.rst").read() + "\n" +
+        open(os.path.join("docs", "HISTORY.txt")).read()
     ),
     classifiers=[
         "Environment :: Web Environment",
