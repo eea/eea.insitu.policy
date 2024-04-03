fa3a96d8f4f5507153f7bd916a05b939d0853a99
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2024-04-03 17:07:36 +0300

    fixed import

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/x-rst",
     long_description=(
         open("README.rst").read() + "\n" +
-        open(os.path.join("docs", "HISTORY.txt")).read()
+        open(join("docs", "HISTORY.txt")).read()
     ),
     classifiers=[
         "Environment :: Web Environment",
