ad05a933ddcf1bd11bbd214e6be115cecb54e711
GhitaB <ghitabizau27@gmail.com>
2024-04-12 13:23:15 +0300

    Refs #267522 - Add file field. Cleanup.

--- eea/insitu/policy/report.py
+++ eea/insitu/policy/report.py
@@ -1,24 +1,21 @@
 """insitu.report content type"""
 
-from plone.app.textfield import RichText
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.restapi.behaviors import IBlocks
 from plone.supermodel import model
 from zope.interface import provider, implementer
-from zope.schema import TextLine
+from plone.namedfile.field import NamedBlobFile
 
 
 @provider(IFormFieldProvider)
 class IInsituReport(model.Schema, IBlocks):
     """InsituReport Interface"""
 
-    testrichtext = RichText(
-        title="Test richtext",
+    file = NamedBlobFile(
+        title="File",
+        description="File containing the report, example: pdf",
         required=False,
     )
-
-    testtextline = TextLine(title="Test textline", required=False)
-    testtextline2 = TextLine(title="Test textline2", required=False)
 
 
 @implementer(IInsituReport)
