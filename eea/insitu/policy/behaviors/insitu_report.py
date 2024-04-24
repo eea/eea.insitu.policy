f051956fba6f6ccb10c5fb8a6fda9b3233c230dd
GhitaB <ghitabizau27@gmail.com>
2024-04-24 16:16:43 +0300

    Refs #268804 - Fix PyLint errors.

--- eea/insitu/policy/behaviors/insitu_report.py
+++ eea/insitu/policy/behaviors/insitu_report.py
@@ -1,10 +1,10 @@
 """Behavior for insitu.report content type"""
 
 from plone.autoform.interfaces import IFormFieldProvider
+from plone.namedfile.field import NamedBlobFile
 from plone.supermodel import model
 from zope import schema
 from zope.interface import provider
-from plone.namedfile.field import NamedBlobFile
 
 
 @provider(IFormFieldProvider)
@@ -21,5 +21,6 @@ class IInsituReportBehavior(model.Schema):
         title="Report category",
         description="Select the category of report",
         required=True,
-        value_type=schema.Choice(vocabulary="eea.insitu.policy.report_categories"),
+        value_type=schema.Choice(
+            vocabulary="eea.insitu.policy.report_categories"),
     )
