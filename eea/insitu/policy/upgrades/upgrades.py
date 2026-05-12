2efaf6e8a42c67d992404254fafd3c4c64e96ac8
Teodor Voicu <teodorvoicu87@gmail.com>
2026-05-12 14:11:53 +0300

    Refs #303431 - Add Sentinel-2 Copernicus component

--- eea/insitu/policy/upgrades/upgrades.py
+++ eea/insitu/policy/upgrades/upgrades.py
@@ -1,1 +1,35 @@
 """Upgrades"""
+
+from collective.taxonomy import PATH_SEPARATOR
+from collective.taxonomy.interfaces import ITaxonomy
+from logging import getLogger
+from zope.component import queryUtility
+
+
+logger = getLogger(__name__)
+
+
+def add_sentinel2_copernicus_component(context):
+    """Add Sentinel-2 to the Copernicus Components taxonomy."""
+    taxonomy = queryUtility(
+        ITaxonomy, name="collective.taxonomy.copernicus_components"
+    )
+
+    if taxonomy is None:
+        logger.warning("Copernicus Components taxonomy was not found.")
+        return
+
+    language = taxonomy.default_language or "en"
+    if language not in taxonomy.data:
+        language = "en"
+
+    identifier = "Sentinel-2"
+    path = PATH_SEPARATOR + "Sentinel-2"
+    existing_identifiers = taxonomy.inverted_data.get(language, {})
+
+    if identifier in existing_identifiers:
+        logger.info("Sentinel-2 already exists in Copernicus Components taxonomy.")
+        return
+
+    taxonomy.update(language, [(path, identifier)], clear=False)
+    logger.info("Added Sentinel-2 to Copernicus Components taxonomy.")
